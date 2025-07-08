51fc32fd276cd64abd32827232e87538101a5aa7
elbrzx <elbrzx@gmail.com>
2025-07-08 23:03:54 +0800

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
-const supabase = createClient(
-  'https://uvwvnxysnyqnvchzzkjg.supabase.co', 
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2d3ZueHlzbnlxbnZjaHp6a2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5ODQ3MTEsImV4cCI6MjA2NzU2MDcxMX0.jxyo6TOoJnnkuhtc-YGjO3cMSjIoR95IUznSDjEB_ko'
-);
+// ganti ini dengan URL dan anon key project Supabase kamu
+const supabaseUrl = 'https://uvwvnxysnyqnvchzzkjg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2d3ZueHlzbnlxbnZjaHp6a2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5ODQ3MTEsImV4cCI6MjA2NzU2MDcxMX0.jxyo6TOoJnnkuhtc-YGjO3cMSjIoR95IUznSDjEB_ko';
+const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 async function handleForm(event) {
   event.preventDefault();
