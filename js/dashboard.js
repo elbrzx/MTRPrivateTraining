010f896f0d321682534fe999d739100983a3f594
elbrzx <elbrzx@gmail.com>
2025-07-08 20:44:01 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,7 @@
 // ============ KONFIG SUPABASE =============
 const SUPABASE_URL = 'https://scjhaagfrzrivnjomvfw.supabase.co'; // GANTI DENGAN PUNYAMU
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjamhhYWdmcnpyaXZuam9tdmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NzAxMzEsImV4cCI6MjA2NzU0NjEzMX0.HvLwUa_G9KrE1Ot8OtIhAhAYX12thSuDNyMZWt9NE9w';       // GANTI DENGAN ANON KEY
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ============ SAAT LOAD PAGE =============
 document.addEventListener("DOMContentLoaded", async () => {
