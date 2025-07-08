d35a455476ae572f620abf3437bef6f6cd379b38
elbrzx <elbrzx@gmail.com>
2025-07-08 23:30:31 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-// ganti ini dengan URL dan anon key project Supabase kamu
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 const supabaseUrl = 'https://uvwvnxysnyqnvchzzkjg.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2d3ZueHlzbnlxbnZjaHp6a2pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5ODQ3MTEsImV4cCI6MjA2NzU2MDcxMX0.jxyo6TOoJnnkuhtc-YGjO3cMSjIoR95IUznSDjEB_ko';
 const supabase = supabase.createClient(supabaseUrl, supabaseAnonKey);
