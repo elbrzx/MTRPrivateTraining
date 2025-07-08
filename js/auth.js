a615832f3c83d99876e583e112053bea5482f70c
elbrzx <elbrzx@gmail.com>
2025-07-08 19:19:19 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -10,4 +10,10 @@ function handleLogin(event) {
   } else {
     alert("Email dan password harus diisi!");
   }
+  // GANTI dengan info kamu dari Supabase
+const SUPABASE_URL = 'https://scjhaagfrzrivnjomvfw.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjamhhYWdmcnpyaXZuam9tdmZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NzAxMzEsImV4cCI6MjA2NzU0NjEzMX0.HvLwUa_G9KrE1Ot8OtIhAhAYX12thSuDNyMZWt9NE9w';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
 }
